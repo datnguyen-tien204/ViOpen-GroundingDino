3cd51055d5272f6be40fae40be7f5ed9165320a5
Dat Nguyen-Tien <118755598+datnguyen-tien204@users.noreply.github.com>
2025-09-21 16:45:58 +0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,12 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-Train and eval functions used in main.py
-"""
-
 import math
 import os
 import sys
 from typing import Iterable
+from tqdm import tqdm
 
 from util.utils import to_device
 import torch
@@ -20,10 +16,9 @@ from datasets.panoptic_eval import PanopticEvaluator
 
 def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
-                    device: torch.device, epoch: int, max_norm: float = 0, 
+                    device: torch.device, epoch: int, max_norm: float = 0,
                     wo_class_error=False, lr_scheduler=None, args=None, logger=None):
     scaler = torch.cuda.amp.GradScaler(enabled=args.amp)
-
 
     model.train()
     criterion.train()
@@ -32,12 +27,11 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     if not wo_class_error:
         metric_logger.add_meter('class_error', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
     header = 'Epoch: [{}]'.format(epoch)
-    print_freq = 10
 
     _cnt = 0
 
-
-    for samples, targets in metric_logger.log_every(data_loader, print_freq, header, logger=logger):
+    progress_bar = tqdm(data_loader, desc=header, leave=True)
+    for samples, targets in progress_bar:
 
         samples = samples.to(device)
         captions = [t["caption"] for t in targets]
@@ -50,7 +44,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             weight_dict = criterion.weight_dict
 
             losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
-        # reduce losses over all GPUs for logging purposes
+
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         loss_dict_reduced_unscaled = {f'{k}_unscaled': v
                                       for k, v in loss_dict_reduced.items()}
@@ -65,7 +59,6 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             print(loss_dict_reduced)
             sys.exit(1)
 
-        # amp backward function
         if args.amp:
             optimizer.zero_grad()
             scaler.scale(losses).backward()
@@ -75,7 +68,6 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
             scaler.step(optimizer)
             scaler.update()
         else:
-            # original backward function
             optimizer.zero_grad()
             losses.backward()
             if max_norm > 0:
@@ -85,11 +77,12 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         if args.onecyclelr:
             lr_scheduler.step()
 
-
         metric_logger.update(loss=loss_value, **loss_dict_reduced_scaled, **loss_dict_reduced_unscaled)
         if 'class_error' in loss_dict_reduced:
             metric_logger.update(class_error=loss_dict_reduced['class_error'])
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        
+        progress_bar.set_postfix(loss=loss_value)
 
         _cnt += 1
         if args.debug:
@@ -102,8 +95,6 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     if getattr(criterion, 'tuning_matching', False):
         criterion.tuning_matching(epoch)
 
-
-    # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
     resstat = {k: meter.global_avg for k, meter in metric_logger.meters.items() if meter.count > 0}
@@ -134,7 +125,6 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
     
     coco_evaluator = CocoGroundingEvaluator(base_ds, iou_types, useCats=useCats)
 
-
     panoptic_evaluator = None
     if 'panoptic' in postprocessors.keys():
         panoptic_evaluator = PanopticEvaluator(
@@ -144,13 +134,12 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         )
 
     _cnt = 0
-    output_state_dict = {} # for debug only
+    output_state_dict = {}
 
     if args.use_coco_eval:
         from pycocotools.coco import COCO
         coco = COCO(args.coco_val_path)
 
-        # 获取所有类别
         category_dict = coco.loadCats(coco.getCatIds())
         cat_list = [item['name'] for item in category_dict]
     else:
@@ -158,7 +147,8 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
     caption = " . ".join(cat_list) + ' .'
     print("Input text prompt:", caption)
 
-    for samples, targets in metric_logger.log_every(data_loader, 10, header, logger=logger):
+    progress_bar = tqdm(data_loader, desc=header, leave=True)
+    for samples, targets in progress_bar:
         samples = samples.to(device)
 
         targets = [{k: to_device(v, device) for k, v in t.items()} for t in targets]
@@ -172,7 +162,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
 
         results = postprocessors['bbox'](outputs, orig_target_sizes)
-        # [scores: [100], labels: [100], boxes: [100, 4]] x B
+
         if 'segm' in postprocessors.keys():
             target_sizes = torch.stack([t["size"] for t in targets], dim=0)
             results = postprocessors['segm'](results, outputs, orig_target_sizes, target_sizes)
@@ -193,21 +183,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
             panoptic_evaluator.update(res_pano)
         
         if args.save_results:
-
-
-
             for i, (tgt, res) in enumerate(zip(targets, results)):
-                """
-                pred vars:
-                    K: number of bbox pred
-                    score: Tensor(K),
-                    label: list(len: K),
-                    bbox: Tensor(K, 4)
-                    idx: list(len: K)
-                tgt: dict.
-
-                """
-                # compare gt and res (after postprocess)
                 gt_bbox = tgt['boxes']
                 gt_label = tgt['labels']
                 gt_info = torch.cat((gt_bbox, gt_label.unsqueeze(-1)), 1)
@@ -217,7 +193,6 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
                 _res_label = res['labels']
                 res_info = torch.cat((_res_bbox, _res_prob.unsqueeze(-1), _res_label.unsqueeze(-1)), 1)
        
-
                 if 'gt_info' not in output_state_dict:
                     output_state_dict['gt_info'] = []
                 output_state_dict['gt_info'].append(gt_info.cpu())
@@ -225,12 +200,6 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
                 if 'res_info' not in output_state_dict:
                     output_state_dict['res_info'] = []
                 output_state_dict['res_info'].append(res_info.cpu())
-
-            # # for debug only
-            # import random
-            # if random.random() > 0.7:
-            #     print("Now let's break")
-            #     break
 
         _cnt += 1
         if args.debug:
@@ -241,13 +210,10 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
     if args.save_results:
         import os.path as osp
         
-        # output_state_dict['gt_info'] = torch.cat(output_state_dict['gt_info'])
-        # output_state_dict['res_info'] = torch.cat(output_state_dict['res_info'])
         savepath = osp.join(args.output_dir, 'results-{}.pkl'.format(utils.get_rank()))
         print("Saving res to {}".format(savepath))
         torch.save(output_state_dict, savepath)
 
-    # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
     if coco_evaluator is not None:
@@ -255,7 +221,6 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
     if panoptic_evaluator is not None:
         panoptic_evaluator.synchronize_between_processes()
 
-    # accumulate predictions from all images
     if coco_evaluator is not None:
         coco_evaluator.accumulate()
         coco_evaluator.summarize()
@@ -274,8 +239,4 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         stats['PQ_th'] = panoptic_res["Things"]
         stats['PQ_st'] = panoptic_res["Stuff"]
 
-
-
     return stats, coco_evaluator
-
-
